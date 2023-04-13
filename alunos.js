db0ea28c7d8b9b0761e627e8b75e30a81014bd66
andradeju <julliana.andrade08@gmail.com>
2023-04-13 20:46:36 -0300

    feat: Criação rota GET alunos com query

---
+++ alunos.js
@@ -0,0 +1,44 @@
+const alunos = [
+    {
+        nome:"Adele",
+        matricula: 1,
+        media: 8.0
+    },
+    {
+        nome:"Beyonce",
+        matricula: 2,
+        media: 10.0
+    },
+    {
+        nome:"Lady Gaga",
+        matricula: 3,
+        media: 9.0
+    },
+    {
+        nome:"Madonna",
+        matricula: 4,
+        media: 7.0
+    },
+    {
+        nome:"Rihanna",
+        matricula: 5,
+        media: 8.0
+    }
+];
+
+
+function filtrarNome(nome){
+    return alunos.filter((el) => 
+    el.nome.toLowerCase().includes(nome.toLowerCase()));
+}
+
+function filtrarMedia(media) {
+    return alunos.filter((el) => 
+    el.media.toFixed(2).includes(media));
+}
+
+module.exports = {
+    alunos,
+    filtrarNome,
+    filtrarMedia
+}
--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require("express");
 const app = express();
+const alunos = require("./alunos");
 
 app.use(express.json());
+
+app.get("/alunos/", (req, res) => {
+
+    let resultadoAlunos = alunos;
+
+    const { nome, media } = req.query;
+
+    if(nome) {
+        resultadoAlunos = resultadoAlunos.filtrarNome (nome);
+    }
+    if(media) {
+        resultadoAlunos = resultadoAlunos.filtrarMedia (media);
+    }
+
+    res.json(resultadoAlunos);
+})
 
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000");
