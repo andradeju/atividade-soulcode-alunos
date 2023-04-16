f48def043861d02907094e731b247d56353e86fb
andradeju <julliana.andrade08@gmail.com>
2023-04-16 12:34:28 -0300

    feat: Escrevendo no db.json toda vez que ocorre alteração nos dados do array de alunos

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const alunos = require("./alunos");
+const fs = require ('fs');
 
 app.use(express.json());
 
@@ -20,12 +21,27 @@ app.get("/alunos/", (req, res) => {
     res.json(resultadoAlunos);
 })
 
+function atualizarAlunos() {
+    const data = JSON.stringify({  alunos }, null, 2 );
+
+    try {
+        fs.writeFileSync("./db.json", data);
+        console.log("Arquivo db.json atualizado com sucesso");
+
+    } catch (error) {
+        console.log("Erro ao atualizar o arquivo:", error);
+    }
+
+}
+
 app.post("/alunos/novo", (req, res) => {
     const { nome, matricula, media } = req.body; 
 
     if(!nome || !matricula || !media) {
         res.status(400).json({ error: "Dados incompletos"})
     } else {
+        alunos.alunos.push({ nome, matricula, media})
+        atualizarAlunos();
         res.json({message: "Aluno adicionado com sucesso"});
     }
 })
@@ -37,6 +53,7 @@ app.post("/alunos/deletar/:index", (req, res) => {
         res.status(404).send({ error: "Aluno não encontrado"})
     } else {
         alunos.alunos.splice(index, 1)
+        atualizarAlunos();
         res.json({ message: "Aluno removido com sucesso"})
     }
 });
@@ -48,10 +65,11 @@ app.post("/alunos/atualizar/:index", (req, res) => {
     if(index < 0 || index >= alunos.alunos.length) {
         res.status(404).send({ error: "Aluno não encontrado"})
     } else if (!nome || !media) {
-        res.status(404).send({ erro: "Dados incompletos"})
+        res.status(404).send({ error: "Dados incompletos"})
     } else {
         alunos.alunos[index].nome = nome;
         alunos.alunos[index].media = media;
+        atualizarAlunos();
         res.json({ message: "Aluno atualizado com sucesso"})
     }
 })
